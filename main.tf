5d2678aebf4764cb6e6dddf085443e33dadb252f
Tom Goodyear <tom.goodyear@red-gate.com>
2023-05-26 09:39:01 +0100

    Fix "latest" is deprecated warning

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "docker_image" "nginx" {
 }
 
 resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
+  image = docker_image.nginx.image_id
   name  = "hello-terraform"
   ports {
     internal = 80
